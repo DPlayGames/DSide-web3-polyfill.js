387c7af04a9e6ef24850e25b76932c0badaff314
Hanul <hanul@hanul.me>
2019-09-28 00:26:25 +0900

    Update DSide-web3-polyfill.js

--- DSide-web3-polyfill.js
+++ DSide-web3-polyfill.js
@@ -59,6 +59,40 @@ else {
 			return false;
 		};
 		
+		let STRINGIFY = (data) => {
+			//REQUIRED: data
+			
+			if (CHECK_IS_DATA(data) === true) {
+				return JSON.stringify(PACK_DATA(data));
+			}
+			
+			else if (CHECK_IS_ARRAY(data) === true) {
+				
+				let f = (array) => {
+					
+					let newArray = [];
+					
+					EACH(array, (data) => {
+						if (CHECK_IS_DATA(data) === true) {
+							newArray.push(PACK_DATA(data));
+						} else if (CHECK_IS_ARRAY(data) === true) {
+							newArray.push(f(data));
+						} else {
+							newArray.push(data);
+						}
+					});
+					
+					return newArray;
+				};
+				
+				return JSON.stringify(f(data));
+			}
+			
+			else {
+				return JSON.stringify(data);
+			}
+		};
+		
 		let EACH = (dataOrArrayOrString, func) => {
 			//OPTIONAL: dataOrArrayOrString
 			//REQUIRED: func
