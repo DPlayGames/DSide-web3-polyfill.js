78bf3a3808a5df683afc257e30afa4dd081a2746
Hanul <hanul@hanul.me>
2019-09-28 19:50:53 +0900

    PACK_DATA 추가

--- DSide-web3-polyfill.js
+++ DSide-web3-polyfill.js
@@ -59,6 +59,50 @@ else {
 			return false;
 		};
 		
+		let PACK_DATA = (data) => {
+			//REQUIRED: data
+	
+			let result = COPY(data);
+			let dateNames = [];
+			let regexNames = [];
+	
+			EACH(result, (value, name) => {
+	
+				if (value instanceof Date === true) {
+	
+					// change to timestamp integer.
+					result[name] = INTEGER(value.getTime());
+					dateNames.push(name);
+				}
+				
+				else if (value instanceof RegExp === true) {
+	
+					// change to string.
+					result[name] = value.toString();
+					regexNames.push(name);
+				}
+	
+				else if (CHECK_IS_DATA(value) === true) {
+					result[name] = PACK_DATA(value);
+				}
+	
+				else if (CHECK_IS_ARRAY(value) === true) {
+	
+					EACH(value, (v, i) => {
+	
+						if (CHECK_IS_DATA(v) === true) {
+							value[i] = PACK_DATA(v);
+						}
+					});
+				}
+			});
+	
+			result.__D = dateNames;
+			result.__R = regexNames;
+	
+			return result;
+		};
+		
 		let STRINGIFY = (data) => {
 			//REQUIRED: data
 			
