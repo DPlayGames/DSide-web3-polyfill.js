e3d5e4e8b3636a66b9cad905ae706e13a3003256
Hanul <hanul@hanul.me>
2019-09-29 15:38:00 +0900

    필요 함수 추가

--- DSide-web3-polyfill.js
+++ DSide-web3-polyfill.js
@@ -59,6 +59,113 @@ else {
 			return false;
 		};
 		
+		let EXTEND = (params) => {
+			//REQUIRED: params
+			//REQUIRED: params.origin	기존 데이터나 배열
+			//REQUIRED: params.extend	덧붙힐 데이터나 배열
+	
+			let origin = params.origin;
+			let extend = params.extend;
+	
+			if (CHECK_IS_DATA(origin) === true) {
+	
+				EACH(extend, (value, name) => {
+					
+					if ( value instanceof Date === true) {
+						origin[name] = new Date(value.getTime());
+					}
+					
+					else if ( value instanceof RegExp === true) {
+						
+						let pattern = value.toString();
+						let flags;
+						
+						for (let i = pattern.length - 1; i >= 0; i -= 1) {
+							if (pattern[i] === '/') {
+								flags = pattern.substring(i + 1);
+								pattern = pattern.substring(1, i);
+								break;
+							}
+						}
+						
+						origin[name] = new RegExp(pattern, flags);
+					}
+					
+					else if (CHECK_IS_DATA(value) === true || CHECK_IS_ARRAY(value) === true) {
+						origin[name] = COPY(value);
+					}
+					
+					else {
+						origin[name] = value;
+					}
+				});
+			}
+	
+			else if (CHECK_IS_ARRAY(origin) === true) {
+	
+				EACH(extend, (value) => {
+	
+					if ( value instanceof Date === true) {
+						origin.push(new Date(value.getTime()));
+					}
+					
+					else if ( value instanceof RegExp === true) {
+						
+						let pattern = value.toString();
+						let flags;
+						
+						for (let i = pattern.length - 1; i >= 0; i -= 1) {
+							if (pattern[i] === '/') {
+								flags = pattern.substring(i + 1);
+								pattern = pattern.substring(1, i);
+								break;
+							}
+						}
+						
+						origin.push(new RegExp(pattern, flags));
+					}
+					
+					else if (CHECK_IS_DATA(value) === true || CHECK_IS_ARRAY(value) === true) {
+						origin.push(COPY(value));
+					}
+					
+					else {
+						origin.push(value);
+					}
+				});
+			}
+	
+			return origin;
+		};
+		
+		let COPY = (dataOrArray) => {
+			//REQUIRED: dataOrArray
+			
+			let copy;
+			
+			if (CHECK_IS_DATA(dataOrArray) === true) {
+	
+				copy = {};
+	
+				EXTEND({
+					origin : copy,
+					extend : dataOrArray
+				});
+			}
+	
+			else if (CHECK_IS_ARRAY(dataOrArray) === true) {
+	
+				copy = [];
+	
+				EXTEND({
+					origin : copy,
+					extend : dataOrArray
+				});
+			}
+	
+			return copy;
+		};
+		
 		let PACK_DATA = (data) => {
 			//REQUIRED: data
 	
