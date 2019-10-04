3e9d88de4eefae36dc09b1bbcb7dfff02f34801f
Hanul <hanul@hanul.me>
2019-10-05 02:35:32 +0900

    버그 수정

--- DSide-web3-polyfill.js
+++ DSide-web3-polyfill.js
@@ -210,6 +210,65 @@ else {
 			return result;
 		};
 		
+		let UNPACK_DATA = (packedData) => {
+			//REQUIRED: packedData	PACK_DATA가 적용된 데이터
+	
+			let result = COPY(packedData);
+	
+			// when date property names exists
+			if (result.__D !== undefined) {
+	
+				// change timestamp integer to Date type.
+				EACH(result.__D, (dateName, i) => {
+					result[dateName] = new Date(result[dateName]);
+				});
+				
+				delete result.__D;
+			}
+			
+			// when regex property names exists
+			if (result.__R !== undefined) {
+	
+				// change string to RegExp type.
+				EACH(result.__R, (regexName, i) => {
+					
+					let pattern = result[regexName];
+					let flags;
+					
+					for (let j = pattern.length - 1; j >= 0; j -= 1) {
+						if (pattern[j] === '/') {
+							flags = pattern.substring(j + 1);
+							pattern = pattern.substring(1, j);
+							break;
+						}
+					}
+					
+					result[regexName] = new RegExp(pattern, flags);
+				});
+				
+				delete result.__R;
+			}
+	
+			EACH(result, (value, name) => {
+	
+				if (CHECK_IS_DATA(value) === true) {
+					result[name] = UNPACK_DATA(value);
+				}
+	
+				else if (CHECK_IS_ARRAY(value) === true) {
+	
+					EACH(value, (v, i) => {
+	
+						if (CHECK_IS_DATA(v) === true) {
+							value[i] = UNPACK_DATA(v);
+						}
+					});
+				}
+			});
+	
+			return result;
+		};
+		
 		let STRINGIFY = (data) => {
 			//REQUIRED: data
 			
